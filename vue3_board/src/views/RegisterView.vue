a89921e2b553260ba5bb0bc0f92d15ca56468e70
nagano <nagano@mirailabs.co.jp>
2022-09-30 15:24:42 +0900

    feat: 登録処理

--- vue3_board/src/views/RegisterView.vue
+++ vue3_board/src/views/RegisterView.vue
@@ -1,9 +1,29 @@
 <script setup lang="ts">
   import { ref } from 'vue';
+  import router from "../router";
 
   const title = ref<string>();
   const description = ref<string>();
   
+  const register = () => {
+    let todoListData = JSON.parse(localStorage.getItem('todo') as string);
+    const maxId: number = todoListData ? Math.max.apply(null,todoListData.map(function(todo){return todo.id;})) : 0;
+
+    const newTodo = {
+        id: maxId + 1,
+        title: title.value,
+        description: description.value
+      };
+      
+    if(todoListData) {
+      todoListData[maxId] = newTodo;
+    } else {
+      todoListData = [newTodo];
+    }
+
+    localStorage.setItem('todo', JSON.stringify(todoListData));
+      router.push({'name': 'list'});
+  }
 </script>
 
 <template>
@@ -12,7 +32,7 @@
       <router-link :to="{name:'list'}">
         <q-btn flat color="primary" label="戻る" />
       </router-link>
-      <q-btn color="primary" label="登録する" />
+      <q-btn color="primary" label="登録する" @click="register"/>
 
 </template>
 
