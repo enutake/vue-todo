33fc548690a993a7ef1edf8488893ba31dc44c5a
nagano <nagano@mirailabs.co.jp>
2022-09-30 15:26:34 +0900

    feat: 動的に表示する

--- vue3_board/src/views/ListView.vue
+++ vue3_board/src/views/ListView.vue
@@ -1,21 +1,7 @@
 <script setup lang="ts">
-    const todoListData = [
-    {
-      'id': 1,
-      'title': '掃除',
-      'description': 'お風呂と部屋の掃除',
-    },
-    {
-      'id': 2,
-      'title': '買い物',
-      'description': 'シャンプーと夜ご飯の食材の購入',
-    },
-    {
-      'id': 3,
-      'title': '銀行',
-      'description': '銀行振込',
-    },
-  ]
+
+  const todoListData = JSON.parse(localStorage.getItem('todo') as string);
+
 </script>
 <template>
       <q-list bordered separator class="todolist">
